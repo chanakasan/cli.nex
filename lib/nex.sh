7bc1ba1cdeebca5603bc8263d3b7e5c3c87c7244
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-01-24 12:32:08 +0530

    rev

--- lib/nex.sh
+++ lib/nex.sh
@@ -1,10 +1,11 @@
 nex_cli() {
-  local a1="$1"
-  if [ -z "$a1" ]; then
+  local verb="$1"
+  local param="$2"
+  if [ -z "$verb" ]; then
     print_usage
-  elif [ "$a1" == "install" ]; then
-    _run_install_script $@
-  elif [ "$a1" == "run" ]; then
+  elif [ "$verb" == "install" ]; then
+    _run_install_script $param
+  elif [ "$verb" == "run" ]; then
     _run_func $@
   else
     print_usage
@@ -30,9 +31,14 @@ _provision() {
 }
 
 _run_install_script() {
-  local full_script_name=nex-$1-$2.sh
-  echo "Running $full_script_name"
-  bash $nex_script_path/src/$full_script_name $3
+  local full_script_name=install-$1.sh
+  local file_path=$nex_script_path/src/$full_script_name
+  if [ -f $file_path ]; then
+    echo "Running $full_script_name"
+    bash  $file_path
+  else
+    echo "script not found: $file_path"
+  fi
 }
 
 _run_func() {
