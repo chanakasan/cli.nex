9597053d3d36b30d8d28552eb66ff0589bec97fa
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-01-29 08:12:09 +0530

    rev

--- lib/nex-cli.sh
+++ lib/nex-cli.sh
@@ -3,15 +3,15 @@ nex_cli() {
   local a1="$1"
   local a2="$2"
   local a3="$3"
-  local fn=_run_$a1
+  local fn=_$a1
   if [ -z "$a1" ]; then
     print_usage
-  elif [ "$a1" == "install" ]; then
-    _run_install_script $a2 $a3
   elif [[ $(type -t $fn ) == function ]]; then
     $fn "${@:2}"
+  elif [ "$a1" == "install" ]; then
+    _run_install_script $a2 $a3
   else
-    _run_other_script $@
+    print_usage
   fi
 }
 
@@ -22,26 +22,19 @@ print_usage() {
 _run_install_script() {
   local full_script_name=install-$1.sh
   local arg1="$2"
-  local file_path=$nex_script_path/src/$full_script_name
-  if [ -f $file_path ]; then
+  local file_path_1=$nex_script_path/src/$full_script_name
+  local file_path_2=$nex_cli_root/src/$full_script_name
+  if [ -f $file_path_1 ]; then
     echo "Running $full_script_name"
-    bash  $file_path $arg1
+    bash  $file_path_1 $arg1
+  elif [ -f $file_path_2 ]; then
+    echo "Running $full_script_name"
+    bash  $file_path_1 $arg1
   else
-    echo "script not found: $file_path"
+    echo "script not found: $full_script_name"
   fi
 }
 
-_run_other_script() {
-  local full_script_name=$1-$2.sh
-  local file_path=$nex_cli_root/src/$full_script_name
-  if [ -f $file_path ]; then
-    echo "Running $full_script_name"
-    bash  $file_path
-  else
-    echo "script not found: $file_path"
-  fi
-}
-
-_run_hello() {
+_hello() {
   echo Hello $1 !
 }
