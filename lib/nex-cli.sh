2919cd38ea24169c1c742a7c10e3ab0585e534b2
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-01-30 22:49:44 +0530

    rev

--- lib/nex-cli.sh
+++ lib/nex-cli.sh
@@ -28,12 +28,8 @@ print_usage() {
 _run_install_script() {
   local full_script_name=install-$1.sh
   local arg1="$2"
-  local file_path_1=$nex_script_path/src/$full_script_name
-  local file_path_2=$nex_cli_root/src/$full_script_name
+  local file_path_1=$devscript_path/src/$full_script_name
   if [ -f $file_path_1 ]; then
-    echo "Running $full_script_name"
-    bash  $file_path_1 $arg1
-  elif [ -f $file_path_2 ]; then
     echo "Running $full_script_name"
     bash  $file_path_1 $arg1
   else
