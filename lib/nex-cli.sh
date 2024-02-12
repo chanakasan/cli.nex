df09c6025aeee12a38cc1ee05b8e92de55e50792
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-02-12 10:00:43 +0530

    rev

--- lib/nex-cli.sh
+++ lib/nex-cli.sh
@@ -1,31 +1,28 @@
 nex_cli() {
   local nex_cli_root=$HOME/dotfiles/nex-cli
+  local main_cmd="nex $1 $2 $3"
   local a1="$1"
   local a2="$2"
   local a3="$3"
-  local fn1=_$a1
+  local fn=_$a1
   local cmd1=_nx_$a1'_'$a2
   local cmd2=_nx_$a1
   if [ -z "$a1" ]; then
-    print_usage
+    echo " usage: nex <command> <arg>"
   elif [ "$a1" == "install" ]; then
-    _run_install_script $a2 $a3
+    run_install_script $a2 $a3
   elif [ "$(is_function $fn)" == "true" ]; then
-    $fn1 "${@:2}"
+    $fn "${@:2}"
   elif [ "$(is_command $cmd1)" == "true" ]; then
     $cmd1 "${@:3}"
   elif [ "$(is_command $cmd2)" == "true" ]; then
     $cmd2 "${@:2}"
   else
-    print_usage
+    echo " not_found: $main_cmd"
   fi
 }
 
-print_usage() {
-  echo " Usage: nex <command> <arg>"
-}
-
-_run_install_script() {
+run_install_script() {
   local full_script_name=install-$1.sh
   local arg1="$2"
   local file_path_1=$devscript_path/src/$full_script_name
@@ -37,31 +34,11 @@ _run_install_script() {
   fi
 }
 
-_reset() {
-  local dir=$1
-  local file=$HOME/dotfiles/$dir/setup/install.sh
-  if [ -z "$dir" ]; then
-    echo aborting
-  elif [ -f $file ]; then
-    echo " Running $file"
-    sh $file
-  else
-    echo " script not found: $file"
-  fi
-}
-
 _hello() {
   echo Hello $1 !
 }
 
-_test_one() {
-  echo Test One !
-}
-
-_test_two() {
-  echo Test One !
-}
-
 _init() {
-  sh $nex_cli_root/lib/nex-init.sh
+  echo " Nex CLI Init"
+  sh $nex_cli_root/lib/nex-cli-init.sh
 }
