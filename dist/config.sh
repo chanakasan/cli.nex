50e8ab3b7e7e833b334699892396a93d7b39dacf
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-04-14 22:04:22 +0530

    rev

--- dist/config.sh
+++ dist/config.sh
@@ -3,3 +3,5 @@ export nex_cli_path=$nex_root_path/cli.nex
 export nex_support=$nex_cli_path/support
 export nex_plugin_base=$nex_root_path/plugins
 export nex_mod_base=$nex_root_path/mods
+export nex_user_mod_base=$HOME/user_mod
+export nex_user_script_path=$HOME/user_script
