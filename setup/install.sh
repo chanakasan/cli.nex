f440429af25101b1bb8bff5b6aaf7da552881824
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-04-18 09:59:56 +0530

    rev

--- setup/install.sh
+++ setup/install.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+$(dirname $0)
 nex_root_path=$HOME/dotfiles
 symlink_src=$nex_root_path/cli.nex/dist/nex
 symlink_dest_dir=/usr/local/bin
@@ -11,12 +12,9 @@ echo " Installing - Nex CLI"
 
 # symlink
 symlink_1=$symlink_dest_dir/nex
-symlink_2=$symlink_dest_dir/nx
 sudo ln -nfs $symlink_src $symlink_1
-sudo ln -nfs $symlink_src $symlink_2
 echo " => created symlinks at $symlink_dest_dir"
 echo " * $symlink_1"
-echo " * $symlink_2"
 
 # finish
 echo " done"
--- setup/uninstall.sh
+++ setup/uninstall.sh
@@ -1,5 +1,4 @@
 echo Uninstalling - Nex CLI
 sudo rm -i /usr/local/bin/nex
-sudo rm -i /usr/local/bin/nx
 echo done
 echo
