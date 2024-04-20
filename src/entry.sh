d821bbfb19e624017a2ffde7530b3d4a00431dd9
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-04-20 15:26:15 +0530

    rev

--- src/entry.sh
+++ src/entry.sh
@@ -1,10 +1,10 @@
-base_dir="$(dirname "${BASH_SOURCE[0]}")/.."
-export PATH=$base_dir/dist:$PATH
-source $(nex _config)
+base_dir="$(dirname "${BASH_SOURCE[0]}")"
+source $base_dir/config.sh
 
 # require_global_var nex_cli_base
 # require_global_var nex_support
 
+export PATH=$nex_cli_base/dist:$PATH
 for f in $nex_cli_base/src/bash/*.sh; do
   if [ -f "$f" ]; then
     source $f
