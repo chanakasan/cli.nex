d11b96a577b2f047249028dc82a0ff8e659bba94
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-04-20 13:12:46 +0530

    rev

--- src/entry.sh
+++ src/entry.sh
@@ -1,1 +1,10 @@
 echo "Nex Cli Ready"
+
+source $(nex _config)
+
+for f in $nex_plugin_base/*.plugin; do
+  tmp_file=$f/src/entry.sh
+  if [ -f "$tmp_file" ]; then
+     source $tmp_file
+  fi
+done
