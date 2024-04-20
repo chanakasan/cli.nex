42a0db0efffb1a98ffddcd148cd8f8a47fd409ab
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-04-20 14:01:32 +0530

    rev

--- setup/install.sh
+++ setup/install.sh
@@ -8,17 +8,18 @@ install_dir=$nex_root/cli.nex
 main() {
   local symlink_src=$install_dir/dist/nex
   local symlink_dest_dir=/usr/local/bin
-  local symlink_1=$symlink_dest_dir/nex
-
-  if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
-    symlink_dest_dir=$HOME/bin
-  fi
 
   echo " Installing - Nex CLI"
-  sudo ln -nfs $symlink_src $symlink_1
+  
+  if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
+    symlink_dest_dir=$HOME/bin
+    ln -nfs $symlink_src $symlink_dest_dir/nex
+  else
+    sudo ln -nfs $symlink_src $symlink_dest_dir/nex
+  fi
+
   echo " => created symlinks"
   echo " * $symlink_1"
-  echo " done"
   echo
 }
 
