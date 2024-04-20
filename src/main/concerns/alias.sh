614741286a938e46c81b3369e0f94e3b6a3eeac8
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-04-21 04:15:31 +0530

    rev

---
+++ src/main/concerns/alias.sh
@@ -0,0 +1,29 @@
+declare -A shortcuts_mapping
+
+shortcuts_mapping=(
+  ["download_git"]="nex plug download git"
+  ["alias"]="vim $nex_cli_base/src/main/concerns/alias.sh"
+)
+
+find_alias() {
+  local key_1=$1'_'$2
+  local key_2=$1
+  local value=$(get_alias $key_1)
+  if [ -n "$value" ]; then
+    echo $value
+  else
+    value=$(get_alias $key_2)
+    echo $value
+  fi
+}
+
+get_alias() {
+  local key=${1:-null}
+  local value=${shortcuts_mapping[$key]}
+  echo $value
+}
+
+has_alias() {
+  local key=$1
+  [[ -n ${shortcuts_mapping[$key]} ]] && return 0 || return 1
+}
