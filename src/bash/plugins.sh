2ef175312cc762335000b18c0084738b9d84ba60
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-04-20 14:05:06 +0530

    rev

---
+++ src/bash/plugins.sh
@@ -0,0 +1,6 @@
+for f in $nex_plugin_base/*.plugin; do
+  tmp_file=$f/src/entry.sh
+  if [ -f "$tmp_file" ]; then
+     source $tmp_file
+  fi
+done
--- src/entry.sh
+++ src/entry.sh
@@ -1,12 +1,5 @@
 source $(nex _config)
 
-for f in $nex_plugin_base/*.plugin; do
-  tmp_file=$f/src/entry.sh
-  if [ -f "$tmp_file" ]; then
-     source $tmp_file
-  fi
-done
-
 for f in $nex_cli_base/src/bash/*.sh; do
   if [ -f "$f" ]; then
      source $f
